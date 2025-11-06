cebb4f430bd14d32fe1906fddc718301d30db66a
BrunoRNS <brunorns05@outlook.com>
2025-11-06 15:12:49 -0300

    Add consoleSetTextCol method to SnesOutput

--- javasnes/src/main/java/javasnes/output/SnesOutput.java
+++ javasnes/src/main/java/javasnes/output/SnesOutput.java
@@ -18,5 +18,18 @@ public class SnesOutput {
             "consoleDrawText(" + x + ", " + y + ", \"" + text + "\");"
         );
     }
+
+    /**
+     * For example: consoleSetTextCol(RGB15(31,0,0), RGB15(0,0,0));
+     * 
+     * @param color
+     * @param bgColor
+     * @return
+     */
+    public static final SnesRawInstruction consoleSetTextCol(String color, String bgColor) {
+        return new SnesRawInstruction(
+            "consoleSetTextCol(" + color + ", " + bgColor + ");"
+        );
+    }
     
 }
